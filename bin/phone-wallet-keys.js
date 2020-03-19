2b3d929170716c7bc259c651da7a399fc8c298b0
Falk Hoppe <falkhoppe81@gmail.com>
2020-03-19 09:11:04 +0100

    move to esm for module management

--- bin/phone-wallet-keys.js
+++ bin/phone-wallet-keys.js
@@ -1,8 +1,11 @@
 #!/usr/bin/env node
-const util = require('util')
-const path = require('path')
-const fs = require('fs-extra')
-const arg = require('arg')
+import util from 'util'
+import { fileURLToPath } from 'url';
+import path from 'path'
+import fs from 'fs-extra'
+import arg from 'arg'
+import { exec as callbackExec } from 'child_process'
+import phoneWalletKeys from '../index.js'
 
 const argOptions = {
   '--module': Boolean,
@@ -48,6 +51,31 @@ ${options.map(o => `  ${o.name}${o.alias ? ` / ${o.alias}` : ''} (${o.type && o.
 `
 }
 
+const __dirname = fileURLToPath(import.meta.url)
+const filesDir = path.resolve(__dirname, '..', '..', 'files')
+
+const tplPath = file => path.resolve(filesDir, file)
+console.log('YYY',tplPath('gitignore'));
+const projectPath = targetPath => path.resolve(process.cwd(), targetPath)
+
+const packageJsonPath = () => path.resolve(process.cwd(), 'package.json')
+
+const packageInfo = async () => {
+  try {
+    const data = await fs.readFile(packageJsonPath())
+    return JSON.parse(data)
+  } catch (e) {
+    console.error(`the current folder does not seem to be a npm project yet.
+
+Please initialize a npm project with \`npm init\``)
+    process.exit(1)
+  }
+}
+
+const createFromFile = (src, target) => fs.copy(tplPath(src), projectPath(target || src))
+
+const exec = util.promisify(callbackExec)
+
 try {
   const args = arg(Object.assign({}, argOptions, aliases))
 
@@ -56,135 +84,12 @@ try {
     process.exit(0)
   }
 
-  const exec = util.promisify(require('child_process').exec)
-
-  const tplPath = file => path.resolve(__dirname, '..', 'files', file)
-  const projectPath = targetPath => path.resolve(process.cwd(), targetPath)
-
-  const packageJsonPath = () => path.resolve(process.cwd(), 'package.json')
-
-  const packageInfo = async () => {
-    try {
-      const data = await fs.readFile(packageJsonPath())
-      return JSON.parse(data)
-    } catch (e) {
-      console.error(`the current folder does not seem to be a npm project yet.
-
-Please initialize a npm project with \`npm init\``)
-      process.exit(1)
-    }
+  const config = {
+    asESMProject: args['--module'] || args['--express'],
+    asExpressApp: args['--express']
   }
 
-  const createFromFile = (src, target) => fs.copy(tplPath(src), projectPath(target || src))
-
-  const addProjectBinary = async () => {
-    const info = await packageInfo()
-    const binaryPath = `bin/${info.name}.js`
-    createFromFile('bin-template.js', binaryPath)
-
-    if (!info.bin) {
-      info.bin = {}
-    }
-
-    if (!info.bin[info.name]) {
-      info.bin[info.name] = binaryPath
-      await fs.outputJson(packageJsonPath(), info, { spaces: 2 })
-    }
-
-    return info
-  }
-
-  const addEslintStandard = async () => {
-    await createFromFile('.eslintrc')
-
-    const installCmd = 'npm i --save-dev eslint eslint-config-standard eslint-plugin-import eslint-plugin-node eslint-plugin-promise eslint-plugin-standard'
-    const { stdout, stderr } = await exec(installCmd, { cwd: process.cwd() })
-    console.log(stdout)
-    console.log(stderr)
-  }
-
-  const addEditorConfig = async () => createFromFile('.editorconfig')
-  const addGitIgnore = async () => createFromFile('gitignore', '.gitignore')
-
-  const addLicenseFile = async () => {
-    const info = await packageInfo()
-    await createFromFile('LICENSE')
-    if (info.license !== 'MIT') {
-      info.license = 'MIT'
-      await fs.outputJson(packageJsonPath(), info, { spaces: 2 })
-    }
-    return info
-  }
-
-  const defineProjectAsModuleBased = async () => {
-    const info = await packageInfo()
-    if (!info.type) {
-      info.type = 'module'
-      info.module = 'index.js'
-      await fs.outputJson(packageJsonPath(), info, { spaces: 2 })
-    }
-    return info
-  }
-
-  const initGit = async () => {
-    const { stdout, stderr } = await exec('git init', { cwd: process.cwd() })
-    console.log(stdout)
-    console.log(stderr)
-  }
-
-  const saveToGit = async message => {
-    const { stdout, stderr } = await exec(`git add -A; git commit -m "${message}"`, { cwd: process.cwd() })
-    console.log(stdout)
-    console.log(stderr)
-  }
-
-  const initializeExpressApp = async () => {
-    const info = await packageInfo()
-    if (!info.scripts.start) {
-      info.scripts.start = `node -r dotenv/config bin/${info.name}.js`
-      info.scripts['dev:generate-self-signed-certs'] = 'mkdir certs; openssl req -nodes -new -x509 -keyout certs/server.key -out certs/server.cert'
-      await fs.outputJson(packageJsonPath(), info, { spaces: 2 })
-    }
-
-    const installCmd = 'npm i express helmet morgan dotenv'
-    const { stdout, stderr } = await exec(installCmd, { cwd: process.cwd() })
-    console.log(stdout)
-    console.log(stderr)
-
-    await createFromFile('express.bin.js', `bin/${info.name}.js`)
-    await createFromFile('express.app.js', 'index.js')
-    await createFromFile('.env.template')
-    await createFromFile('.env.template', '.env')
-
-    return info
-  }
-
-  const bootstrap = async () => {
-    try {
-      await initGit()
-      await addGitIgnore()
-      await addProjectBinary()
-      await addEditorConfig()
-      await addLicenseFile()
-
-      // npm is updating the package json again, so do it last
-      await addEslintStandard()
-      await saveToGit('initialize empty js project')
-
-      if (args['--module'] || args['--express']) {
-        await defineProjectAsModuleBased()
-        await saveToGit('define app as module based')
-      }
-
-      if (args['--express']) {
-        await initializeExpressApp()
-        await saveToGit('initialize express app')
-      }
-    } catch (e) {
-      console.error(e)
-      process.exit(1)
-    }
-  }
+  const bootstrap = phoneWalletKeys({ packageInfo, packageJsonPath }, { createFromFile, exec }, config)
 
   bootstrap()
     .then(() => console.log(`
---
+++ index.js
@@ -0,0 +1,115 @@
+import fs from 'fs-extra'
+
+export default (npmPackage, utils, config) => {
+  const addProjectBinary = async () => {
+    const info = await npmPackage.packageInfo()
+
+    const binaryPath = `bin/${info.name}.js`
+    utils.createFromFile('bin-template.js', binaryPath)
+
+    if (!info.bin) {
+      info.bin = {}
+    }
+
+    if (!info.bin[info.name]) {
+      info.bin[info.name] = binaryPath
+      await fs.outputJson(npmPackage.packageJsonPath(), info, { spaces: 2 })
+    }
+
+    return info
+  }
+
+  const addEslintStandard = async () => {
+    await utils.createFromFile('.eslintrc')
+
+    const installCmd = 'npm i --save-dev eslint eslint-config-standard eslint-plugin-import eslint-plugin-node eslint-plugin-promise eslint-plugin-standard'
+    const { stdout, stderr } = await utils.exec(installCmd, { cwd: process.cwd() })
+    console.log(stdout)
+    console.log(stderr)
+  }
+
+  const addEditorConfig = async () => utils.createFromFile('.editorconfig')
+  const addGitIgnore = async () => utils.createFromFile('gitignore', '.gitignore')
+
+  const addLicenseFile = async () => {
+    const info = await npmPackage.packageInfo()
+    await utils.createFromFile('LICENSE')
+    if (info.license !== 'MIT') {
+      info.license = 'MIT'
+      await fs.outputJson(npmPackage.packageJsonPath(), info, { spaces: 2 })
+    }
+    return info
+  }
+
+  const defineProjectAsModuleBased = async () => {
+    const info = await npmPackage.packageInfo()
+    if (!info.type) {
+      info.type = 'module'
+      info.module = 'index.js'
+      await fs.outputJson(npmPackage.packageJsonPath(), info, { spaces: 2 })
+    }
+    return info
+  }
+
+  const initGit = async () => {
+    const { stdout, stderr } = await utils.exec('git init', { cwd: process.cwd() })
+    console.log(stdout)
+    console.log(stderr)
+  }
+
+  const saveToGit = async message => {
+    const { stdout, stderr } = await utils.exec(`git add -A; git commit -m "${message}"`, { cwd: process.cwd() })
+    console.log(stdout)
+    console.log(stderr)
+  }
+
+  const initializeExpressApp = async () => {
+    const info = await npmPackage.packageInfo()
+    if (!info.scripts.start) {
+      info.scripts.start = `node -r dotenv/config bin/${info.name}.js`
+      info.scripts['dev:generate-self-signed-certs'] = 'mkdir certs; openssl req -nodes -new -x509 -keyout certs/server.key -out certs/server.cert'
+      await fs.outputJson(npmPackage.packageJsonPath(), info, { spaces: 2 })
+    }
+
+    const installCmd = 'npm i express helmet morgan dotenv'
+    const { stdout, stderr } = await utils.exec(installCmd, { cwd: process.cwd() })
+    console.log(stdout)
+    console.log(stderr)
+
+    await utils.createFromFile('express.bin.js', `bin/${info.name}.js`)
+    await utils.createFromFile('express.app.js', 'index.js')
+    await utils.createFromFile('.env.template')
+    await utils.createFromFile('.env.template', '.env')
+
+    return info
+  }
+
+  const bootstrap = async () => {
+    try {
+      await initGit()
+      await addGitIgnore()
+      await addProjectBinary()
+      await addEditorConfig()
+      await addLicenseFile()
+
+      // npm is updating the package json again, so do it last
+      await addEslintStandard()
+      await saveToGit('initialize empty js project')
+
+      if (config.asESMProject) {
+        await defineProjectAsModuleBased()
+        await saveToGit('define app as module based')
+      }
+
+      if (config.asExpressApp) {
+        await initializeExpressApp()
+        await saveToGit('initialize express app')
+      }
+    } catch (e) {
+      console.error(e)
+      process.exit(1)
+    }
+  }
+
+  return bootstrap
+}
