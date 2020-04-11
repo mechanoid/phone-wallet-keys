4d24fd97a6328e4a7b2cb600044b28f1a95a91d2
Falk Hoppe <falkhoppe81+git@gmail.com>
2020-04-11 11:40:23 +0200

    add shebang line in express bin file

--- files/express.bin.js
+++ files/express.bin.js
@@ -1,6 +1,8 @@
-import { readFileSync } from 'fs'
-import { createServer } from 'https'
-import app from '../index.js'
+#!/usr/bin/env node
+
+import { readFileSync } from "fs";
+import { createServer } from "https";
+import app from "../index.js";
 
 const config = {
   port: process.env.PORT,
@@ -8,15 +10,19 @@ const config = {
     key: process.env.SSL_KEY_PATH,
     cert: process.env.SSL_CERT_PATH
   }
-}
+};
 
-const listener = config.ssl.key && config.ssl.cert
-  ? createServer({
-    key: readFileSync(config.ssl.key),
-    cert: readFileSync(config.ssl.cert)
-  }, app(config))
-  : app(config)
+const listener =
+  config.ssl.key && config.ssl.cert
+    ? createServer(
+        {
+          key: readFileSync(config.ssl.key),
+          cert: readFileSync(config.ssl.cert)
+        },
+        app(config)
+      )
+    : app(config);
 
 const server = listener.listen(config.port, () => {
-  console.log(`listening to port: ${server.address().port}`)
-})
+  console.log(`listening to port: ${server.address().port}`);
+});
